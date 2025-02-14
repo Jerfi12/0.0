d1f30040de98e0b71bacc59fb1ceca3fe091dfe3
Jerfi12 <dima.dima.stepanov79@mail.ru>
2025-02-14 09:11:56 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 38;
+            int c = (31 - 5 * a) / b;
+            Console.WriteLine(c);
+        }
+    }
+}
